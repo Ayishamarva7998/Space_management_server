4267918185eadee8eb4df1d986c58def6a8ca2de
nizamudheenknr <nizamudheenknr@gmail.com>
2024-09-21 11:16:59 +0530

    feat

--- Server.js
+++ Server.js
@@ -1,44 +1,37 @@
-import express from "express"
-import mongoose from "mongoose"
-import Dotenv from "dotenv"
-import cors from "cors"
-import multer from "multer"; 
-import adminroute from "./routes/admin_route.js"
-import loginroute from "./routes/auth.js"
-import staffroute from './routes/staff_route.js'
+import express from "express";
+import mongoose from "mongoose";
+import Dotenv from "dotenv";
+import cors from "cors";
+import multer from "multer";
+import adminroute from "./routes/admin_route.js";
+import loginroute from "./routes/auth.js";
+import staffroute from "./routes/staff_route.js";
 
-
-
-
-Dotenv.config()
-const server = express()
+Dotenv.config();
+const server = express();
 const upload = multer();
-server.use(express.json())
-server.use(cors())
 
+server.use(express.json());
 
-
+server.use(cors());
+server.use(
+  cors({
+    origin: "https://space-management.vercel.app",
+  })
+);
 
 server.use("/api/admin", adminroute);
 server.use("/api", loginroute);
 server.use("/api", staffroute);
 
-
-
 // Local Host
-const PORT = process.env.PORT 
-server.listen(PORT,()=>{
-    console.log(`Server Running in Port ${PORT}`)
-})
-
-
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server Running in Port ${PORT}`);
+});
 
 // Connecting DB
-mongoose.connect(process.env.DB,{dbName:"space-management"})
+mongoose
+  .connect(process.env.DB, { dbName: "space-management" })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-
-
-
-
+  .catch((err) => console.log(err));
