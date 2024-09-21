158a51f87cb8b4c47f6af08d9d42391a7fb74d31
nizamudheenknr <nizamudheenknr@gmail.com>
2024-09-21 12:14:52 +0530

    fix error

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,5 @@
 import { Admin } from "../models/admin.js";
-import { genarate_admin_token } from "../utils/jwt.js";
+// import { genarate_admin_token } from "../utils/jwt.js";
 
 export const login = async (req,res)=>{
     const {email,password}=req.body;
@@ -17,7 +17,7 @@ export const login = async (req,res)=>{
           return res.status(404).json({message:"Not found the Admin"});
         };
 
-        return res.status(201).json({message:"logged successfully",token:genarate_admin_token(admin)})
+        // return res.status(201).json({message:"logged successfully",token:genarate_admin_token(admin)})
       
       
     } catch (error) {
