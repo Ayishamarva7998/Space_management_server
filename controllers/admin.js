5eaf84250783a81beaf6c9597599ffa91c48da08
nizamudheenknr <nizamudheenknr@gmail.com>
2024-09-21 13:16:10 +0530

    fix error

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,28 +1,30 @@
 import { Admin } from "../models/admin.js";
 
 export const createAdmin = async (req, res) => {
-    const { name, email, password } = req.body;
-  
-  try {
-    
+  const { name, email, password } = req.body;
 
+  try {
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-  
     const existingadmin = await Admin.findOne({ email });
     if (existingadmin) {
-      return res.status(409).json({ message: "Admin with this email already exists" });
+      return res
+        .status(409)
+        .json({ message: "Admin with this email already exists" });
     }
 
-  
     const newadmin = new Admin({ name, email, password });
 
     await newadmin.save();
 
-    return res.status(201).json({ message: "Admin created successfully", admin: newadmin });
+    return res
+      .status(201)
+      .json({ message: "Admin created successfully", admin: newadmin });
   } catch (error) {
-    return res.status(500).json({ message: "Server error", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
   }
 };
--- controllers/login.js
+++ controllers/login.js
@@ -1,28 +1,23 @@
 import { Admin } from "../models/admin.js";
 import { genarate_admin_token } from "../utils/jwt.js";
 
-export const login = async (req,res)=>{
-    const {email,password}=req.body;
-    
-    try {
+export const login = async (req, res) => {
+  const { email, password } = req.body;
 
-        if(!email || !password){
-            return res.status(400).json({ message: "All fields are required" });
-        };
-
-        const admin = await Admin.findOne({email}); 
-        
-        
-        if(!admin){
-          return res.status(404).json({message:"Not found the Admin"});
-        };
-        const token = genarate_admin_token(admin);
-        return res.status(201).json({message:"logged successfully",token})
-        
-      
-    } catch (error) {
-        return res.status(500).json({message:"internal error"})
-      
-        
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-}
+
+    const admin = await Admin.findOne({ email });
+
+    if (!admin) {
+      return res.status(404).json({ message: "Not found the Admin" });
+    }
+    const token = genarate_admin_token(admin);
+    return res.status(201).json({ message: "logged successfully", token });
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res.status(500).json({ message: "internal error" });
+  }
+};
